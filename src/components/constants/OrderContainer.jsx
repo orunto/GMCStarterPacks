9783cf7d43166db5b0fd36b5b5a96e543fe9e1ee
chayilDeborah <aniyikayeolamide3@gmail.com>
2022-07-26 01:41:17 +0100

    modified the price tag

--- src/components/constants/OrderContainer.jsx
+++ src/components/constants/OrderContainer.jsx
@@ -5,7 +5,7 @@ import { PriceContainer, Price } from '../styles/Components.styles';
 export const OrderContainer = () => {
   return (
     <PriceContainer>
-    <Price>Order Pack for N170000</Price>
+    <Price>Order Pack for ₦170,000</Price>
   </PriceContainer>
 
   )
